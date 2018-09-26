e9db8dfff37f125fe71aaa609dd22034db82fff3
Vintage <ms_wwl@163.com>
2018-09-26 17:56:49 +0800

    mybatis

---
+++ datahelper/src/main/java/com/vintage/datahelper/DataHelperApplication.java
@@ -0,0 +1,12 @@
+package com.vintage.datahelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataHelperApplication.class, args);
+    }
+}
